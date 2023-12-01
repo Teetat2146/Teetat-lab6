fc7eeadd5abf2e41ec45fb54f2d8271d7d19fe48
Teetat <teetatjj1@gmail.com>
2023-12-01 16:27:06 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,26 @@
 using namespace std;
 
 int main(){
+    
+    int x =1;
+    int even = 0 , odd = 0 ;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while( x != 0)
+    {
+      
+      
+      if( x%2 == 0){
+          even = even+1;
+        }else{
+             odd = odd+1;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+    }          
+    
+    cout << "#Even numbers = " << even << "\n" ;
+    cout << "#Odd numbers = " << odd ;
     return 0;
+
 }
